c968043813fe5ef9a6928cc23d32899e2fcb93ad
TomTobin <tomtobin0@gmail.com>
2022-11-03 22:49:32 +0000

    Initial commit

---
+++ app/src/main/java/ie/wit/contribution/Contribute.kt
@@ -0,0 +1,11 @@
+package ie.wit.contribution
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Contribute : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contribute)
+    }
+}
