71a6815855b6b04442b24d04542bb8d5ddfb66ab
TomTobin <tomtobin0@gmail.com>
2022-11-13 09:11:39 +0000

    Home activity fix

--- app/src/main/java/ie/wit/contribution/activities/Home.kt
+++ app/src/main/java/ie/wit/contribution/activities/Home.kt
@@ -2,11 +2,33 @@ package ie.wit.contribution.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import ie.wit.contribution.R
+import ie.wit.contribution.databinding.HomeBinding
+
+private lateinit var drawerLayout: DrawerLayout
+private lateinit var homeBinding : HomeBinding
 
 class Home : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home)
+
+        //initialize drawerLayout
+        homeBinding = HomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+        drawerLayout = homeBinding.drawerLayout
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    }
+    // handle Navigation up/back
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
